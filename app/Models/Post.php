abf9fcb64eebc44282685bee1376a21f77d1d8a3
GiadaTonioni <giada.tonioni@gmail.com>
2023-09-04 17:38:08 +0200

    foreign key

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,12 +6,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use Illuminate\Support\Str;
+use App\Model\Type;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title', 'content', 'slug'];
+
+    public function type(){
+        return $this->belongsTo(Type::class);
+    }
 
     public static function generateSlug($title)
     {
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -5,7 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Model\Post;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 class Type extends Model
 {
     use HasFactory;
+    protected $fillable = ['name', 'slug'];
+
+    public function posts(): HasMany{
+        return $this->hasMany(Post::class);
+    }
 }
